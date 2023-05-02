df49493decd4ee5efdad3ed610c5522cfb652233
Sultan AlAmoudi <solttan00100@gmail.com>
2023-05-02 18:13:17 +0300

    First commit

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyDgO7YL-UljdzmSOZk2dq3BM5IgyycToJM",
+  authDomain: "capstone-project-app-10682.firebaseapp.com",
+  projectId: "capstone-project-app-10682",
+  storageBucket: "capstone-project-app-10682.appspot.com",
+  messagingSenderId: "671583139842",
+  appId: "1:671583139842:web:051fdb392f76fe54744f1b",
+  measurementId: "G-1Y3N9KV6FJ"
+};
