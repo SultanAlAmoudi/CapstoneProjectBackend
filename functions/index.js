732f1a635e452370966623fd1767a5aa7a78b899
Sultan AlAmoudi <solttan00100@gmail.com>
2023-05-19 21:36:08 +0300

    deleted unwanted functions

--- functions/index.js
+++ functions/index.js
@@ -25,16 +25,8 @@ const {
   markNotificationsRead,
 } = require("./handlers/users");
 
-// Scream routes
-/* app.get("/screams", getAllScreams);
-app.post("/scream", FBAuth, postOneScream);
-app.get("/scream/:screamId", getScream);
-app.delete("/scream/:screamId", FBAuth, deleteScream);
-app.get("/scream/:screamId/like", FBAuth, likeScream);
-app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
-app.post("/scream/:screamId/comment", FBAuth, commentOnScream); */
-
-app.get("/requests", getAllRequests);
+// data routes
+app.get("/requests", FBAuth, getAllRequests);
 app.post("/request", FBAuth, postRequest);
 app.post("/request/:requestId/offer", FBAuth, offerOnRequest);
 app.get("/offers/:requestId", FBAuth, getOffers);
@@ -51,70 +43,7 @@ app.post("/notifications", FBAuth, markNotificationsRead);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
 
-/* exports.createNotificationOnLike = functions
-  .region("europe-west1")
-  .firestore.document("likes/{id}")
-  .onCreate((snapshot) => {
-    return db
-      .doc(`/screams/${snapshot.data().screamId}`)
-      .get()
-      .then((doc) => {
-        if (
-          doc.exists &&
-          doc.data().userHandle !== snapshot.data().userHandle
-        ) {
-          return db.doc(`/notifications/${snapshot.id}`).set({
-            createdAt: new Date().toISOString(),
-            recipient: doc.data().userHandle,
-            sender: snapshot.data().userHandle,
-            type: "like",
-            read: false,
-            screamId: doc.id,
-          });
-        }
-      })
-      .catch((err) => console.error(err));
-  });
-exports.deleteNotificationOnUnLike = functions
-  .region("europe-west1")
-  .firestore.document("likes/{id}")
-  .onDelete((snapshot) => {
-    return db
-      .doc(`/notifications/${snapshot.id}`)
-      .delete()
-      .catch((err) => {
-        console.error(err);
-        return;
-      });
-  });
-exports.createNotificationOnComment = functions
-  .region("europe-west1")
-  .firestore.document("comments/{id}")
-  .onCreate((snapshot) => {
-    return db
-      .doc(`/screams/${snapshot.data().screamId}`)
-      .get()
-      .then((doc) => {
-        if (
-          doc.exists &&
-          doc.data().userHandle !== snapshot.data().userHandle
-        ) {
-          return db.doc(`/notifications/${snapshot.id}`).set({
-            createdAt: new Date().toISOString(),
-            recipient: doc.data().userHandle,
-            sender: snapshot.data().userHandle,
-            type: "comment",
-            read: false,
-            screamId: doc.id,
-          });
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        return;
-      });
-  });
-
+/*
 exports.onUserImageChange = functions
   .region("europe-west1")
   .firestore.document("/users/{userId}")
@@ -138,37 +67,4 @@ exports.onUserImageChange = functions
     } else return true;
   });
 
-exports.onScreamDelete = functions
-  .region("europe-west1")
-  .firestore.document("/screams/{screamId}")
-  .onDelete((snapshot, context) => {
-    const screamId = context.params.screamId;
-    const batch = db.batch();
-    return db
-      .collection("comments")
-      .where("screamId", "==", screamId)
-      .get()
-      .then((data) => {
-        data.forEach((doc) => {
-          batch.delete(db.doc(`/comments/${doc.id}`));
-        });
-        return db.collection("likes").where("screamId", "==", screamId).get();
-      })
-      .then((data) => {
-        data.forEach((doc) => {
-          batch.delete(db.doc(`/likes/${doc.id}`));
-        });
-        return db
-          .collection("notifications")
-          .where("screamId", "==", screamId)
-          .get();
-      })
-      .then((data) => {
-        data.forEach((doc) => {
-          batch.delete(db.doc(`/notifications/${doc.id}`));
-        });
-        return batch.commit();
-      })
-      .catch((err) => console.error(err));
-  });
  */
